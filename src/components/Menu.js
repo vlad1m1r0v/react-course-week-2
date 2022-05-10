a4cc2127a62fc56437fd50280c695575136cde1c
= <mypochtav@gmail.com>
2022-05-10 13:48:22 +0300

    split into smaller fucntional components

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,20 @@
 import { Card } from "react-bootstrap";
 
+function RenderMenuItem({ dish, onCLick }) {
+  return (
+    <Card key={dish.id} onClick={() => onCLick(dish.id)}>
+      <Card.Img src={dish.image} alt={dish.name} />
+      <Card.ImgOverlay>
+        <Card.Title>{dish.name}</Card.Title>
+      </Card.ImgOverlay>
+    </Card>
+  );
+}
+
 export default function Menu({ dishes, onClick }) {
   const menu = dishes.map((dish) => (
     <div className="col-12 col-md-5 m-1">
-      <Card key={dish.id} onClick={() => onClick(dish.id)}>
-        <Card.Img src={dish.image} alt={dish.name} />
-        <Card.ImgOverlay>
-          <Card.Title>{dish.name}</Card.Title>
-        </Card.ImgOverlay>
-      </Card>
+      <RenderMenuItem dish={dish} onCLick={onClick} />
     </div>
   ));
 
