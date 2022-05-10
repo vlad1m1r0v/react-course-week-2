17bc030ec243ee84236ef0a6b80d0657c07b1692
= <mypochtav@gmail.com>
2022-05-10 20:45:30 +0300

    complete hometask for week 2

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,15 @@
-import { Card } from "react-bootstrap";
+import { Card, Breadcrumb, BreadcrumbItem } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function RenderMenuItem({ dish, onCLick }) {
   return (
-    <Card key={dish.id} onClick={() => onCLick(dish.id)}>
-      <Card.Img src={dish.image} alt={dish.name} />
-      <Card.ImgOverlay>
-        <Card.Title>{dish.name}</Card.Title>
-      </Card.ImgOverlay>
+    <Card key={dish.id}>
+      <Link to={`/menu/${dish.id}`}>
+        <Card.Img src={dish.image} alt={dish.name} />
+        <Card.ImgOverlay>
+          <Card.Title>{dish.name}</Card.Title>
+        </Card.ImgOverlay>
+      </Link>
     </Card>
   );
 }
@@ -20,6 +23,18 @@ export default function Menu({ dishes, onClick }) {
 
   return (
     <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/home">Home</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>Menu</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>Menu</h3>
+          <hr />
+        </div>
+      </div>
       <div className="row">{menu}</div>
     </div>
   );
