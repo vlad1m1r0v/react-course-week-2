ceb89907a9f709c7fa7559df7066db325c030004
= <mypochtav@gmail.com>
2022-05-10 17:58:39 +0300

    add content in home page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,12 @@
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 import DishDetail from "./DishDetail";
 import Menu from "./Menu";
 import Header from "./Header";
 import Footer from "./Footer";
+import Contact from "./Contact";
 import { useState } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./Home";
@@ -10,13 +14,22 @@ import Home from "./Home";
 export default function Main() {
   const [state, setState] = useState({
     dishes: DISHES,
+    comments: COMMENTS,
+    promotions: PROMOTIONS,
+    leaders: LEADERS,
     selectedDish: null,
   });
 
   const onDishSelect = (dishId) => setState({ ...state, selectedDish: dishId });
 
   const HomePage = () => {
-    return <Home />;
+    return (
+      <Home
+        dish={state.dishes.find((dish) => dish.featured)}
+        promotion={state.promotions.find((promo) => promo.featured)}
+        leader={state.leaders.find((leader) => leader.featured)}
+      />
+    );
   };
 
   return (
@@ -29,11 +42,12 @@ export default function Main() {
           path="/menu"
           element={<Menu dishes={state.dishes} onClick={onDishSelect} />}
         />
+        <Route exact path="/contactus" element={<Contact />} />
         <Route path="*" element={<Navigate to="/home" replace />} />
       </Routes>
-      <DishDetail
+      {/* <DishDetail
         dish={state.dishes.find((dish) => dish.id === state.selectedDish)}
-      />
+      /> */}
       <Footer />
     </>
   );
